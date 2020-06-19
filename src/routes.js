1c9778c48a0060bb3e0d0c25e707d0509098451e
Rafael Borges <=>
2020-06-19 00:32:55 -0300

    add middlewares

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const routes = express.Router()
-const {uuid} = require('uuidv4') //cria um id unico
+const {uuid, isUuid} = require('uuidv4') //cria um id unico
 
 routes.use(express.json())
 
 const projects = []
 
-routes.get('/projects', (request, response) => {
+function logRequest(request, response, next) {
+  const { method, url } = request 
+  const logLabel = `method: ${method} url: ${url}`
+  console.log(logLabel)
+
+  return next() //com o next a gente tem um retorno, sem ele tem a interrupção da requisição (prox middeware)
+
+}
+
+function validadeProjectId(request, response, next) {
+  const { id } = request.params
+  if(!isUuid(id)) { //se nao for valida o Id
+    return response.status(400).json({error:"id is not valid"})
+  }
+
+  return next() //caso seja valido chama a request
+
+}
+
+routes.use(logRequest)
+
+routes.use('/projects/:id',validadeProjectId) //para essas rotas aplicar esse middlare
+
+routes.get('/projects', logRequest, (request, response) => {
   const { title } = request.query
 
   const resoluts = title   //se tem algo na var title
@@ -32,9 +55,6 @@ routes.get('/projects', (request, response) => {
 
   const idFinded = projects.findIndex(project => project.id == id )// pega o positivo
 
-//  if(idFinded < 0) {
-  //return response.status('400').json({error: "id not found"})
-  //}
   const project = {
     id,
     title,
