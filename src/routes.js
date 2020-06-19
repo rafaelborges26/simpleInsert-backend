508a703597dcacd907f5d426aea51f3ba071ae37
Rafael Borges <rafael.borges2698@gmail.com>
2020-06-18 23:10:16 -0300

    add routes

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,66 @@
 const express = require('express')
-
 const routes = express.Router()
+const {uuid} = require('uuidv4') //cria um id unico
 
+routes.use(express.json())
 
-routes.get('/projects', (require, response) => {
-    return response.json({message: "teste"})
-     
+const projects = []
+
+routes.get('/projects', (request, response) => {
+  const { title } = request.query
+
+  const resoluts = title   //se tem algo na var title
+  ? projects.filter(project => project.title.includes(title))  //se tiver pegamos   
+  : projects //se n tiver queue params mostramos td
+  
+    return response.json({resoluts})
+})  
+  
+
+ routes.post('/projects', (request, response) => {
+  const { title, owner } = request.body
+
+  const project = {id: uuid(), title, owner} //id unico
+  projects.push(project)
+  //console.log(project)
+  return response.json(project)
  })
 
- routes.post('/projects', (require, response) => {
-   return response.json({message: "teste"})
+ routes.put('/projects/:id', (request, response) => {
+   const { id } = request.params //pegando da url
+  const { title, owner } = request.body //pegando do corpo em json
+
+  const idFinded = projects.findIndex(project => project.id == id )// pega o positivo
+
+//  if(idFinded < 0) {
+  //return response.status('400').json({error: "id not found"})
+  //}
+  const project = {
+    id,
+    title,
+    owner
+  } 
+
+  projects[idFinded] = project
+
+  return response.json(project)
  })
 
- routes.put('/projects/:id', (require, response) => {
-   return response.json({message: "teste"})
- })
+ routes.delete('/projects/:id', (request, response) => {
+  const { id } = request.params
+  const { title, owner } = request.body
+  
+  const idprofectFounded = projects.findIndex(project => project.id == id )
 
- routes.delete('/projects/:id', (require, response) => {
-   return response.json({message: "teste"})
+  if(idprofectFounded < 0) {
+
+    return response.status("400").json({error: "id not founded"})
+
+  }
+
+
+  projects.splice(projects[idprofectFounded], 1)
+  
+  return response.status(204).json({message: "vazio"})//status de vazio
  })
 module.exports = routes
